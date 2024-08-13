03702766957a792db072e53104d9f75f481c495d
Donsimeone2206 <simeon.mustapha@gmail.com>
2024-08-13 13:09:03 -0700

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,3 +10,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
