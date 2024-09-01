b44ab95808394a23da7916b1b583ff3e0dc62f5e
Donsimeone2206 <simeon.mustapha@gmail.com>
2024-09-01 14:49:05 -0700

    api project

---
+++ api_project/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+
+router = DefaultRouter()
+router.register(r'Book' ,BookViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+ 
+]
