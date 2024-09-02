3420f96e3f107ad9e383f10dcd5fb0cc7bbcf9fb
Donsimeone2206 <simeon.mustapha@gmail.com>
2024-09-02 10:33:24 -0700

    Task 4

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,44 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
+from .models import Author, Book
+
+class BookAPITestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.author = Author.objects.create(name="John Doe")
+        self.book = Book.objects.create(title="Test Book", publication_year=2020, author=self.author)
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_book(self):
+        url = reverse('book-create')
+        data = {'title': 'New Book', 'publication_year': 2021, 'author': self.author.id}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+        self.assertEqual(Book.objects.get(id=2).title, 'New Book')
+
+    def test_update_book(self):
+        url = reverse('book-update', kwargs={'pk': self.book.id})
+        data = {'title': 'Updated Book', 'publication_year': 2021, 'author': self.author.id}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, 'Updated Book')
+
+    def test_delete_book(self):
+        url = reverse('book-delete', kwargs={'pk': self.book.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_list_books(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('Test Book' in str(response.data))
+
+# Additional tests can be added for filtering, searching, ordering, and permissions.
+
