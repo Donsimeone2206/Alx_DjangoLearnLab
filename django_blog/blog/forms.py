c7978c401d7ab7ff31509b587dace1e39b76cf2a
Donsimeone2206 <simeon.mustapha@gmail.com>
2024-09-14 02:52:18 -0700

    task 4 update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content']
         widgets = {
-            'tags': TagWidge(),
+            'tags': TagWidget(),
         }
 
 class CommentForm(forms.ModelForm):
