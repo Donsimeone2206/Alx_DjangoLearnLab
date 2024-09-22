69ae3d40c7985ee5db6e732cf8fb4a334d8c2203
Donsimeone2206 <simeon.mustapha@gmail.com>
2024-09-22 02:46:13 -0700

    social_media_api

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers')
+        read_only_fields = ('followers',)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
